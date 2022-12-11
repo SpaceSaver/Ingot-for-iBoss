93c5815ff8712d0d322961948f76d29d9aacaacd
SpaceSaver <spacesaverundercoverdev@gmail.com>
2022-12-11 17:53:19 -0500

    More helpful title and error message

--- ingot.js
+++ ingot.js
@@ -20,7 +20,7 @@ function isButtonDev() {
 
 //Set base page code
 document.documentElement.innerHTML = `<html><head><link rel="icon" href="data:image/svg+xml,<svg viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' version='1.1'><path fill='white' d='M20.5 11H19V7c0-1.1-.9-2-2-2h-4V3.5C13 2.12 11.88 1 10.5 1S8 2.12 8 3.5V5H4c-1.1 0-1.99.9-1.99 2v3.8H3.5c1.49 0 2.7 1.21 2.7 2.7s-1.21 2.7-2.7 2.7H2V20c0 1.1.9 2 2 2h3.8v-1.5c0-1.49 1.21-2.7 2.7-2.7 1.49 0 2.7 1.21 2.7 2.7V22H17c1.1 0 2-.9 2-2v-4h1.5c1.38 0 2.5-1.12 2.5-2.5S21.88 11 20.5 11z'></path></svg>">
-<title>Ingot</title>
+<title>Ingot for iBoss</title>
 </head>
 <body ` + isPageDev() + `>
 <div class="nav">
@@ -43,7 +43,7 @@ document.documentElement.innerHTML = `<html><head><link rel="icon" href="data:im
 <div class="items-main">
 <div class="items" id="items">
 <div class="patched">Error: This may have been patched</div>
-<div class="wrongpage">You are not on the correct page.<br>To use Ingot for iBoss click the button below to redirect click the \"Click me!\".<div class="item-left-buttons" style="justify-content: center; margin: 20px;">
+<div class="wrongpage">You are not on the correct page.<br>To use Ingot for iBoss click the button below to redirect click the \"Click me!\" button.  Once on the blank page, click the bookmarklet again.<div class="item-left-buttons" style="justify-content: center; margin: 20px;">
 <div class="item-left-button">Redirect</div>
 </div></div>
 </div>
